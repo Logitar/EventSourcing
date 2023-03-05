3e8dbc71fe75934e3dc2eb377f15fce583ac1693
Francis Pion <francispion@hotmail.com>
2023-03-04 22:14:04 -0500

    ListExtensions.AddIfNotNull now returns a boolean. (#33)

--- src/Logitar.EventSourcing/ListExtensions.cs
+++ src/Logitar.EventSourcing/ListExtensions.cs
@@ -7,15 +7,21 @@ public static class ListExtensions
 {
   /// <summary>
   /// Adds the specified item to the specified list, only if its not null. Otherwise, nothing happens.
+  /// Returns a value indicating whether or not the specified item was added to the specified list.
   /// </summary>
   /// <typeparam name="T">The type of the list items.</typeparam>
   /// <param name="list">The list.</param>
   /// <param name="item">The item.</param>
-  public static void AddIfNotNull<T>(this List<T> list, T? item)
+  /// <returns>True if the item was added, false otherwise (if item was null).</returns>
+  public static bool AddIfNotNull<T>(this List<T> list, T? item)
   {
-    if (item != null)
+    if (item == null)
     {
-      list.Add(item);
+      return false;
     }
+
+    list.Add(item);
+
+    return true;
   }
 }
