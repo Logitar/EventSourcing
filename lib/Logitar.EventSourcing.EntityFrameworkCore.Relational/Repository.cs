e551e997a29ba1f123079b736a174301b0f531a1
Francis Pion <francispion@hotmail.com>
2024-12-03 14:29:06 -0500

    save

---
+++ lib/Logitar.EventSourcing.EntityFrameworkCore.Relational/Repository.cs
@@ -0,0 +1,39 @@
+﻿using Logitar.EventSourcing.Infrastructure;
+
+namespace Logitar.EventSourcing.EntityFrameworkCore.Relational;
+
+/// <summary>
+/// Represents an interface that allows retrieving and storing events in an Entity Framework Core relational event store.
+/// </summary>
+public class Repository : Infrastructure.Repository
+{
+  /// <summary>
+  /// Gets the event database context.
+  /// </summary>
+  protected EventContext EventContext { get; }
+
+  /// <summary>
+  /// Initializes a new instance of the <see cref="Repository"/> class.
+  /// </summary>
+  /// <param name="eventBus">The event bus.</param>
+  /// <param name="eventContext">The event database context.</param>
+  /// <param name="eventSerializer">The serializer for events.</param>
+  public Repository(IEventBus eventBus, EventContext eventContext, IEventSerializer eventSerializer) : base(eventBus, eventSerializer)
+  {
+    EventContext = eventContext;
+  }
+
+  /// <summary>
+  /// Persists a list of aggregate changes to the event store.
+  /// </summary>
+  /// <param name="aggregates">The list of aggregates to persist their changes.</param>
+  /// <param name="cancellationToken">The cancellation token.</param>
+  /// <returns>The asynchronous operation.</returns>
+  protected override async Task SaveChangesAsync(IEnumerable<IAggregate> aggregates, CancellationToken cancellationToken)
+  {
+    IEnumerable<EventEntity> events = aggregates.SelectMany(aggregate => EventEntity.FromChanges(aggregate, EventSerializer));
+
+    EventContext.Events.AddRange(events);
+    await EventContext.SaveChangesAsync(cancellationToken);
+  }
+}
---
+++ lib/Logitar.EventSourcing.Infrastructure/Repository.cs
@@ -0,0 +1,80 @@
+﻿namespace Logitar.EventSourcing.Infrastructure;
+
+/// <summary>
+/// Represents an interface that allows retrieving and storing events in an event store.
+/// </summary>
+public abstract class Repository : IRepository
+{
+  /// <summary>
+  /// Initializes a new instance of the <see cref="Repository"/> class.
+  /// </summary>
+  /// <param name="eventBus">The event bus to publish the events to.</param>
+  /// <param name="eventSerializer">The serializer for events.</param>
+  protected Repository(IEventBus eventBus, IEventSerializer eventSerializer)
+  {
+    EventBus = eventBus;
+    EventSerializer = eventSerializer;
+  }
+
+  /// <summary>
+  /// Gets the event bus to publish the events to.
+  /// </summary>
+  protected IEventBus EventBus { get; }
+  /// <summary>
+  /// Gets the serializer for events.
+  /// </summary>
+  protected IEventSerializer EventSerializer { get; }
+
+  /// <summary>
+  /// Persists an aggregate to the event store.
+  /// </summary>
+  /// <param name="aggregate">The aggregate to persist.</param>
+  /// <param name="cancellationToken">The cancellation token.</param>
+  /// <returns>The asynchronous operation.</returns>
+  public virtual async Task SaveAsync(IAggregate aggregate, CancellationToken cancellationToken)
+  {
+    await SaveAsync([aggregate], cancellationToken);
+  }
+  /// <summary>
+  /// Persists a list of aggregates to the event store.
+  /// </summary>
+  /// <param name="aggregates">The list of aggregates to persist.</param>
+  /// <param name="cancellationToken">The cancellation token.</param>
+  /// <returns>The asynchronous operation.</returns>
+  public virtual async Task SaveAsync(IEnumerable<IAggregate> aggregates, CancellationToken cancellationToken)
+  {
+    await SaveChangesAsync(aggregates, cancellationToken);
+
+    await PublishAndClearChangesAsync(aggregates, cancellationToken);
+  }
+
+  /// <summary>
+  /// Persists a list of aggregate changes to the event store.
+  /// </summary>
+  /// <param name="aggregates">The list of aggregates to persist their changes.</param>
+  /// <param name="cancellationToken">The cancellation token.</param>
+  /// <returns>The asynchronous operation.</returns>
+  protected abstract Task SaveChangesAsync(IEnumerable<IAggregate> aggregates, CancellationToken cancellationToken);
+
+  /// <summary>
+  /// Publishes to the event bus and clear the changes of the specified aggregates.
+  /// </summary>
+  /// <param name="aggregates">The list of aggregates to publish their events.</param>
+  /// <param name="cancellationToken">The cancellation token.</param>
+  /// <returns>The asynchronous operation.</returns>
+  protected virtual async Task PublishAndClearChangesAsync(IEnumerable<IAggregate> aggregates, CancellationToken cancellationToken)
+  {
+    foreach (IAggregate aggregate in aggregates)
+    {
+      if (aggregate.HasChanges)
+      {
+        foreach (IEvent change in aggregate.Changes)
+        {
+          await EventBus.PublishAsync(change, cancellationToken);
+        }
+
+        aggregate.ClearChanges();
+      }
+    }
+  }
+}
