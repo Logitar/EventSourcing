559d95770f53fab99f57386f0a4966636874e969
Francis Pion <francispion@hotmail.com>
2024-12-03 13:20:36 -0500

    repo

---
+++ lib/Logitar.EventSourcing/IRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace Logitar.EventSourcing;
+
+public interface IRepository
+{
+  /// <summary>
+  /// Persists an aggregate to the event store.
+  /// </summary>
+  /// <param name="aggregate">The aggregate to persist.</param>
+  /// <param name="cancellationToken">The cancellation token.</param>
+  /// <returns>The asynchronous operation.</returns>
+  Task SaveAsync(IAggregate aggregate, CancellationToken cancellationToken = default);
+  /// <summary>
+  /// Persists a list of aggregates to the event store.
+  /// </summary>
+  /// <param name="aggregates">The list of aggregates to persist.</param>
+  /// <param name="cancellationToken">The cancellation token.</param>
+  /// <returns>The asynchronous operation.</returns>
+  Task SaveAsync(IEnumerable<IAggregate> aggregates, CancellationToken cancellationToken = default);
+}
