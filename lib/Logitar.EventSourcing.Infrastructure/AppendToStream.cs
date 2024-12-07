52d134cb090b15068a4a90978d619b54c295ec24
Francis Pion <francispion@hotmail.com>
2024-12-07 15:54:29 -0500

    Append

---
+++ lib/Logitar.EventSourcing.Infrastructure/AppendToStream.cs
@@ -0,0 +1,42 @@
+﻿namespace Logitar.EventSourcing.Infrastructure;
+
+/// <summary>
+/// Represents an operation that apprends events to a stream.
+/// </summary>
+public record AppendToStream
+{
+  /// <summary>
+  /// Gets the stream identifier.
+  /// </summary>
+  public StreamId Id { get; }
+
+  /// <summary>
+  /// Gets the stream type.
+  /// </summary>
+  public Type? Type { get; }
+
+  /// <summary>
+  /// Gets the stream expectation.
+  /// </summary>
+  public StreamExpectation Expectation { get; }
+
+  /// <summary>
+  /// Gets the events to append.
+  /// </summary>
+  public IEnumerable<object> Events { get; }
+
+  /// <summary>
+  /// Initializes a new instance of the <see cref="AppendToStream"/> class.
+  /// </summary>
+  /// <param name="id">The stream identifier.</param>
+  /// <param name="type">The stream type.</param>
+  /// <param name="expectation">The stream expectation.</param>
+  /// <param name="events">The events to append.</param>
+  public AppendToStream(StreamId id, Type? type, StreamExpectation expectation, IEnumerable<object> events)
+  {
+    Id = id;
+    Type = type;
+    Expectation = expectation;
+    Events = events;
+  }
+}
---
+++ lib/Logitar.EventSourcing.Kurrent/EventStore.cs
@@ -0,0 +1,61 @@
+﻿using Logitar.EventSourcing.Infrastructure;
+
+namespace Logitar.EventSourcing.Kurrent;
+
+/// <summary>
+/// TODO(fpion): document
+/// </summary>
+public sealed class EventStore : IEventStore
+{
+  /// <summary>
+  /// The uncommitted operations.
+  /// </summary>
+  private readonly List<AppendToStream> _operations = [];
+
+  /// <summary>
+  /// Appends the specified events to the specified stream.
+  /// </summary>
+  /// <param name="streamId">The stream identifier (optional).</param>
+  /// <param name="type">The stream type (optional).</param>
+  /// <param name="expectation">An expectation of the stream state.</param>
+  /// <param name="events">The events to append.</param>
+  /// <returns>The stream identifier. A new identifier will be randomly generated if none is provided.</returns>
+  /// <exception cref="ArgumentException">The stream identifier was null, empty or only white-space.</exception>
+  public StreamId Append(StreamId? streamId, Type? type, StreamExpectation expectation, IEnumerable<object> events)
+  {
+    if (streamId.HasValue)
+    {
+      if (string.IsNullOrWhiteSpace(streamId.Value.Value))
+      {
+        throw new ArgumentException("The stream identifier cannot be null, empty, nor only white-space.", nameof(streamId));
+      }
+    }
+    else
+    {
+      streamId = StreamId.NewId();
+    }
+
+    if (events.Any())
+    {
+      AppendToStream operation = new(streamId.Value, type, expectation, events);
+      _operations.Add(operation);
+    }
+
+    return streamId.Value;
+  }
+
+  /// <summary>
+  /// Save the changes to the event store.
+  /// </summary>
+  /// <param name="cancellationToken">The cancellation token.</param>
+  /// <returns>The asynchronous operation.</returns>
+  public async Task SaveChangesAsync(CancellationToken cancellationToken)
+  {
+    if (_operations.Count < 1)
+    {
+      return;
+    }
+
+    await Task.Delay(0, cancellationToken); // TODO(fpion): implement
+  }
+}
