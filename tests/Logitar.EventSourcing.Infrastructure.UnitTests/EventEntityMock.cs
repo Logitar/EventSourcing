f78c5b1ca06869888ae1b0f33c70d71cbd3ccf7a
Francis Pion <francispion@hotmail.com>
2024-12-07 14:53:27 -0500

    tests

--- tests/Logitar.EventSourcing.Infrastructure.UnitTests/EventEntityMock.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Logitar.EventSourcing.Infrastructure;
-
-internal class EventEntityMock : IEventEntity
-{
-  public string Id { get; init; } = string.Empty;
-
-  public string EventType { get; init; } = string.Empty;
-  public string EventData { get; init; } = string.Empty;
-}
