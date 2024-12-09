68ab4a392dfe0fb5688e03df43f9009234844954
Francis Pion <francispion@hotmail.com>
2024-12-09 00:32:41 -0500

    entities

---
+++ lib/Logitar.EventSourcing/Event.cs
@@ -0,0 +1,78 @@
+﻿namespace Logitar.EventSourcing;
+
+/// <summary>
+/// TODO(fpion): document
+/// </summary>
+public class Event
+{
+  /// <summary>
+  /// Gets the identifier of the event.
+  /// </summary>
+  public EventId Id { get; } // TODO(fpion): nullable?
+  /// <summary>
+  /// Gets the type of the event.
+  /// </summary>
+  public Type? Type { get; } // TODO(fpion): nullable?
+  /// <summary>
+  /// Gets the version of the event.
+  /// </summary>
+  public long? Version { get; } // TODO(fpion): nullable?
+
+  /// <summary>
+  /// Gets the identifier of the actor who raised the event.
+  /// </summary>
+  public ActorId? ActorId { get; }
+  /// <summary>
+  /// Gets the date and time when the event was raised.
+  /// </summary>
+  public DateTime? OccurredOn { get; } // TODO(fpion): nullable?
+  /// <summary>
+  /// Gets a value indicating whether or not the stream is deleted, undeleted or unchanged.
+  /// </summary>
+  public bool? IsDeleted { get; }
+
+  /// <summary>
+  /// Gets the event data.
+  /// </summary>
+  public IEvent Data { get; }
+
+  /// <summary>
+  /// Initializes a new instance of the <see cref="Event"/> class.
+  /// </summary>
+  /// <param name="id">The identifier of the event.</param>
+  /// <param name="data">The event data.</param>
+  /// <param name="type">The type of the event.</param>
+  /// <param name="version">The version of the event.</param>
+  /// <param name="actorId">The identifier of the actor who raised the event.</param>
+  /// <param name="occurredOn">The date and time when the event was raised.</param>
+  /// <param name="isDeleted">A value indicating whether or not the stream is deleted, undeleted or unchanged.</param>
+  public Event(EventId id, IEvent data, Type? type = null, long? version = null, ActorId? actorId = null, DateTime? occurredOn = null, bool? isDeleted = null)
+  {
+    Id = id;
+    Type = type;
+    Version = version;
+
+    ActorId = actorId;
+    OccurredOn = occurredOn;
+    IsDeleted = isDeleted;
+
+    Data = data;
+  }
+
+  /// <summary>
+  /// Returns a value indicating whether or not the specified object is equal to the event.
+  /// </summary>
+  /// <param name="obj">The object to be compared to.</param>
+  /// <returns>True if the object is equal to the event.</returns>
+  public override bool Equals(object obj) => obj is Event @event && @event.Id == Id;
+  /// <summary>
+  /// Returns the hash code of the current event.
+  /// </summary>
+  /// <returns>The hash code.</returns>
+  public override int GetHashCode() => Id.GetHashCode();
+  /// <summary>
+  /// Returns a string representation of the event.
+  /// </summary>
+  /// <returns>The string representation.</returns>
+  public override string ToString() => Type == null ? Id.Value : $"{Type} (Id={Id})";
+}
---
+++ lib/Logitar.EventSourcing/Stream.cs
@@ -0,0 +1,97 @@
+﻿using System.Linq;
+
+namespace Logitar.EventSourcing;
+
+/// <summary>
+/// Represents a stream of events.
+/// </summary>
+public class Stream
+{
+  /// <summary>
+  /// Gets the identifier of the stream.
+  /// </summary>
+  public StreamId Id { get; }
+  /// <summary>
+  /// Gets the type of the stream.
+  /// </summary>
+  public Type? Type { get; }
+  /// <summary>
+  /// Gets the version of the stream.
+  /// </summary>
+  public long Version { get; }
+
+  /// <summary>
+  /// Gets the identifier of the actor who created the stream.
+  /// </summary>
+  public ActorId? CreatedBy { get; }
+  /// <summary>
+  /// Gets the date and time when the stream was created.
+  /// </summary>
+  public DateTime? CreatedOn { get; }
+
+  /// <summary>
+  /// Gets the identifier of the actor who updated the stream lastly.
+  /// </summary>
+  public ActorId? UpdatedBy { get; }
+  /// <summary>
+  /// Gets the date and time when the stream was updated lastly.
+  /// </summary>
+  public DateTime? UpdatedOn { get; }
+
+  /// <summary>
+  /// Gets a value indicating whether or not the stream is deleted.
+  /// </summary>
+  public bool IsDeleted { get; }
+
+  /// <summary>
+  /// Gets the events in the stream.
+  /// </summary>
+  public IReadOnlyCollection<Event> Events { get; }
+
+  /// <summary>
+  /// Initializes a new instance of the <see cref="Stream"/> class.
+  /// </summary>
+  /// <param name="id">The stream identifier.</param>
+  /// <param name="type">The stream type.</param>
+  /// <param name="version">The stream version.</param>
+  /// <param name="createdBy">The identifier of the actor who created the stream.</param>
+  /// <param name="createdOn">The date and time when the stream was created.</param>
+  /// <param name="updatedBy">The identifier of the actor who updated the stream lastly.</param>
+  /// <param name="updatedOn">The date and time when the stream was updated lastly.</param>
+  /// <param name="isDeleted">A value indicating whether or not the stream is deleted.</param>
+  /// <param name="events">The events in the stream.</param>
+  public Stream(StreamId id, Type? type = null, long version = 0, ActorId? createdBy = null, DateTime? createdOn = null,
+    ActorId? updatedBy = null, DateTime? updatedOn = null, bool isDeleted = false, IEnumerable<Event>? events = null)
+  {
+    Id = id;
+    Type = type;
+    Version = version;
+
+    CreatedBy = createdBy;
+    CreatedOn = createdOn;
+
+    UpdatedBy = updatedBy;
+    UpdatedOn = updatedOn;
+
+    IsDeleted = isDeleted;
+
+    Events = events?.ToArray() ?? [];
+  }
+
+  /// <summary>
+  /// Returns a value indicating whether or not the specified object is equal to the stream.
+  /// </summary>
+  /// <param name="obj">The object to be compared to.</param>
+  /// <returns>True if the object is equal to the stream.</returns>
+  public override bool Equals(object obj) => obj is Stream stream && stream.Id == Id;
+  /// <summary>
+  /// Returns the hash code of the current stream.
+  /// </summary>
+  /// <returns>The hash code.</returns>
+  public override int GetHashCode() => Id.GetHashCode();
+  /// <summary>
+  /// Returns a string representation of the stream.
+  /// </summary>
+  /// <returns>The string representation.</returns>
+  public override string ToString() => Type == null ? Id.Value : $"{Type} (Id={Id})";
+}
