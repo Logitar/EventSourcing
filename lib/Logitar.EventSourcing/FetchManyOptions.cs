d27bf1cbfccd9723c9f4ce2d82057ca66f578eb4
Francis Pion <francispion@hotmail.com>
2024-12-11 21:11:31 -0500

    FetchManyOptions

--- lib/Logitar.EventSourcing/FetchManyOptions.cs
+++ lib/Logitar.EventSourcing/FetchManyOptions.cs
@@ -3,4 +3,15 @@
 /// <summary>
 /// Represents options for fetching many streams of events.
 /// </summary>
-public record FetchManyOptions;
+public record FetchManyOptions : FetchOptions
+{
+  /// <summary>
+  /// Gets or sets the list of stream types to filter by. If no type is specified, this filter will be ignored.
+  /// </summary>
+  public List<Type> StreamTypes { get; set; } = [];
+
+  /// <summary>
+  /// Gets or sets the list of stream identifiers to filter by. If no identifier is specified, this filter will be ignored.
+  /// </summary>
+  public List<StreamId> StreamIds { get; set; } = [];
+}
