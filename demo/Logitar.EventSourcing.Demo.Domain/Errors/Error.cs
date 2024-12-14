8c9cfb493e0496da68f3d7e753e416736314ecb4
Francis Pion <francispion@hotmail.com>
2024-12-14 00:07:33 -0500

    errors

---
+++ demo/Logitar.EventSourcing.Demo.Domain/Errors/Error.cs
@@ -0,0 +1,32 @@
+﻿namespace Logitar.EventSourcing.Demo.Domain.Errors;
+
+public record Error
+{
+  public string Code { get; set; } = string.Empty;
+  public string Message { get; set; } = string.Empty;
+  public List<ErrorData> Data { get; set; } = [];
+
+  public Error()
+  {
+  }
+
+  public Error(string code, string message, IEnumerable<ErrorData>? data = null)
+  {
+    Code = code;
+    Message = message;
+
+    if (data != null)
+    {
+      Data.AddRange(data);
+    }
+  }
+
+  public void AddData(KeyValuePair<string, object?> data)
+  {
+    Data.Add(new ErrorData(data));
+  }
+  public void AddData(string key, object? value)
+  {
+    Data.Add(new ErrorData(key, value));
+  }
+}
