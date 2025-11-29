7a06873556faa89ba5b5d64800f875aa997ac0ab
Francis Pion <francispion@hotmail.com>
2025-11-29 14:29:01 -0500

    Added `IEventHandler`.

---
+++ lib/Logitar.EventSourcing/IEventHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Logitar.EventSourcing;
+
+/// <summary>
+/// Represents a handler for a specific event.
+/// </summary>
+/// <typeparam name="TEvent">The type of the event.</typeparam>
+public interface IEventHandler<TEvent> where TEvent : IEvent
+{
+  /// <summary>
+  /// Handles the specified event.
+  /// </summary>
+  /// <param name="event">The event to handle.</param>
+  /// <param name="cancellationToken">The cancellation token.</param>
+  /// <returns>The asynchronous operation.</returns>
+  Task HandleAsync(TEvent @event, CancellationToken cancellationToken = default);
+}
